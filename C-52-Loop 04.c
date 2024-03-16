b5e3f8a4e97aaeec53121c2c05d83b8f9ed331d4
RH Rimon <83422826+RH-Rimon@users.noreply.github.com>
2024-03-16 23:09:59 +0600

    Add files via upload

---
+++ C-52-Loop 04.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i = 1;
+
+    while(i<=10){
+
+        printf("%d\n",i);
+        i++;
+    }
+
+
+
+
+    return 0;
+}
---
+++ C-53-Loop 05 While Loop Awesome.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+
+    while(1){
+
+        printf("Enter a Number: \n");
+    scanf("%d",&num1);
+    printf("Enter Another Number: \n");
+    scanf("%d",&num2);
+
+    printf("Sum is: %d\n",num1+num2);
+
+    }
+    return 0;
+}
